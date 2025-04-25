b7051d7d8bd9a6eb39000623bf6102abfbc6b6a9
Pedro <pedrolucasgoncalvespedro@gmail.com>
2025-04-25 17:27:18 -0300

    feat: add AI search context and create README

---
+++ README.md
@@ -0,0 +1,54 @@
+# 🖥️🤖 RushAI
+Uma IA Generativa especialista no time da Furia de CS para te ajudar a obter informações de forma rápida.
+
+## O que ela faz?
+Com o RushAI você pode fazer qualquer pergunta relacionada ao time de Counter Strike da Furia.
+
+
+
+<div align="center">
+   <img align="center" height="450" width="550" src="https://i.imgur.com/nmARMeT.png">
+</div>
+
+## IA Utlizada:
+Gemini
+
+## Bibliotecas Utilizadas:
+* Requests - Ferramenta simples e elegante projetada para fazer requisições HTTP de maneira eficiente e fácil de usar. Ela permite interagir com APIs da web, acessar páginas da web, enviar dados para servidores e muito mais.Tornando o processo de comunicação entre seu programa Python e a web mais direto e eficiente.
+
+* Langchain_google_genai - Fornece acesso a embeddings gerados por IA (Inteligência Artificial) da Google para aplicações de processamento de linguagem natural (NLP).
+
+* Streamlit - Biblioteca de código aberto em Python projetada para facilitar a criação de aplicativos web interativos para ciência de dados e aprendizado de máquina.
+
+## Instruções para Rodar:
+Primeiramente, é necessário clonar o repositório:
+
+```bash
+git clone https://github.com/pedrolucasgp/chatbot-furioso.git
+```
+
+* Este projeto utiliza o Virtual Environment (venv) para gerenciar um ambiente virtual para a instalação das dependências.
+
+* Para criar o venv e instalar as dependências, abra o Prompt de Comando (cmd) dentro do projeto e utilize a sequência de comandos a seguir:
+
+```bash
+python -m venv venv
+```
+
+```bash
+venv\Scripts\activate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+* E por fim, utilize o seguinte comando para rodar o projeto:
+
+```bash
+streamlit run main.py
+```
+
+## Pronto!
+ 
+* O projeto abrirá automaticamente em seu navegador, mas caso não abra, será fornecido o link de acesso no console.
--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-from google import genai
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts.chat import ChatPromptTemplate
+from PyPDF2 import PdfReader
 
 load_dotenv()
 
 avatar = "https://i.imgur.com/QnTYTCw.png"
 user = "https://upload.wikimedia.org/wikipedia/pt/f/f9/Furia_Esports_logo.png"
+pdf = PdfReader("Furia_DataBase.pdf")
 
 llm = ChatGoogleGenerativeAI(
-    temperature=0.2,
+    temperature=0.8,
     model="gemini-2.0-flash",
-    max_tokens=100,
+    max_tokens=1000,
     api_key=os.getenv("SECRET_KEY")
 )
 
+def read_pdf():
+    text = ""
+
+    for page in pdf.pages:
+        text += page.extract_text()
+    return text
+
 def chatbot_interaction(question):
+    context = read_pdf()
+
     prompt = ChatPromptTemplate.from_messages([
-        ("system", f"""Seu nome é RushAI e você é um especialista em e-sports, mais especificamente sobre a Furia.
+        ("system", f"""Seu nome é RushAI e você é um especialista em e-sports, mais especificamente sobre a organização chamada Furia.
          Você deve responder as perguntas focando no time de CS2 da Furia, masculino e feminino.
          Você deve responder as perguntas de forma clara e objetiva.
-         Você deve sempre buscar as notícias atualizadas nas fontes mais confiáveis e atualizadas levando em consideração a data da resposta, como o site oficial da Furia, Twitter, Instagram, HLTV, Dust2, Draft5, entre outros.
+         Você deve usar como fonte principal esse PDF: {context}.
+         Você deve usar informações que não estão no PDF, de sites como HLTV, Liquipedia, Dust2, e redes sociais da Furia.
          Você deve sempre responder em português, mesmo se a pergunta for em inglês.
-         Você deve sempre deve ser direto, politico e amigável, mesmo se a pergunta for ofensiva ou provocativa.
-         Você não deve inventar informações, mesmo se não souber a resposta.
+         Você deve analisar a pergunta e resposta anterior para responder de forma mais precisa a pergunta atual.
+         Você deve sempre deve ser politico e amigável, mesmo se a pergunta for ofensiva ou provocativa.
+         Você NUNCA deve inventar informações, em hipótese alguma, mesmo se não souber a resposta.
          Você não deve cometer erros de português, mesmo se a pergunta tiver erros de português.
          Você não deve deixar respostas em aberto, ou incompletas, mesmo se a pergunta for aberta ou incompleta.
          """),
         ("user", question),
     ])
     prompt = prompt.format_messages(question=question)
-    response = llm(prompt)
+    response = llm.invoke(prompt)
 
     return response.content
 
@@ -46,7 +58,7 @@ st.markdown(
     <div style="text-align: center;">
         <div style=" display: flex; justify-content: center; align-items: center; margin: 20px; gap: 20px;">
             <img src={avatar} width="150">
-            <h1>Rush AI</h1>
+            <h1>RushAI</h1>
         </div>
         <h5>Olá jogador! Sou o RushAI, a IA mais furiosa e especialista sobre nossa Furia!</h5><br><br>
     </div>
@@ -58,7 +70,7 @@ if "messages" not in st.session_state:
 for message in st.session_state["messages"]:
     st.chat_message(message["role"], avatar=avatar if message["role"] == "assistant" else user).write(message["content"])
 
-if prompt := st.chat_input():
+if prompt := st.chat_input("Digite sua pergunta aqui..."):
     st.session_state["messages"].append({"role": "user", "content": prompt})
     st.chat_message("user", avatar=user).write(prompt)
 
